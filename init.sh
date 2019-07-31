dd7a6fb83f2beb6422787c94776bb458741ccbfd
maany <imptodefeat@gmail.com>
2019-07-31 16:24:34 +0200

    update crontab paths

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "*/${REFRESH_INTERVAL} * * * * root cd /code && python3 manage.py fetch_data >> /code/fetch_data_output" >> /etc/crontab
+echo "*/${REFRESH_INTERVAL} * * * * root /usr/bin/python3 /code/manage.py fetch_data >> /code/fetch_data_output" >> /etc/crontab
 service cron start
 python3 manage.py runserver 0.0.0.0:8000
