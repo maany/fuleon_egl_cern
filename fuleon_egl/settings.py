4fbdff01566dc30a98d4ebaaedddd2b63f8ae78c
maany <imptodefeat@gmail.com>
2019-09-23 23:55:01 +0200

    gunicorn whitenoise refactor

--- fuleon_egl/settings.py
+++ fuleon_egl/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'fuleon_egl.urls'
@@ -118,8 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MODE = config('MODE', default='local')
 
 if MODE == 'local':
